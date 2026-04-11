d779c37151ccefcbaf4eea7a3f51c33549d13a1e
Muhammad Awais <muhammadawaisgill18@gmail.com>
2026-04-11 12:44:29 +0500

    latest commit

--- lib/providers/template_provider.dart
+++ lib/providers/template_provider.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final selectedTemplateProvider = StateProvider<int>((ref) => 1);
+final selectedVariantProvider = StateProvider<Color?>((ref) => null);
